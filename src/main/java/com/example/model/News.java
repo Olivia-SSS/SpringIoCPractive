32ca99a79d1292a61b060ca37051a65f41198182
Olivia-SSS <songjunt@seas.upenn.edu>
2024-04-02 20:56:13 -0400

    final ver

--- src/main/java/com/example/model/News.java
+++ src/main/java/com/example/model/News.java
@@ -6,12 +6,10 @@ public class News {
     private String theme; // New field for theme
     private List<Press> pressReleases;
 
-    // Existing constructor that takes a List of Press objects
     public News(List<Press> pressReleases) {
         this.pressReleases = pressReleases;
     }
 
-    // New getter and setter for the theme
     public String getTheme() {
         return theme;
     }
@@ -20,7 +18,6 @@ public class News {
         this.theme = theme;
     }
 
-    // Existing getter and setter for pressReleases
     public List<Press> getPressReleases() {
         return pressReleases;
     }
